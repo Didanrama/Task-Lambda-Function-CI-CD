b8705b1616d571e345ab90f76456273223dc911e
Didanrama <didanramadhan035@gmail.com>
2024-08-30 22:10:14 +0700

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello World! PERUBAHAN SECARA OTOMATIS SETELAH COMMIT')
+        'body': json.dumps('Hello World!')
     }
